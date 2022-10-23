b4752e0107c81d96cfcdd066d78cb6fad1e1c505
issei.ueda <ron.ron633662@gmail.com>
2022-10-23 11:55:53 +0900

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Issei Ueda on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//I am Rich
+
+}
+
